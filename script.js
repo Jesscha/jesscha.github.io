a281e4e08fb34954b2497e21b63b92b4653fe103
jessecha <jessecha222@gmail.com>
2019-12-22 18:55:13 +0900

    이미지 변환

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-
-
 //checked인 상황이면, 사진이 갈아끼워지는 걸 만들자
 
 //div로 묶고 디스플레이를 none으로 했다가 visible로 했다가를 바꾸고 해야겠다.
 
-//
+
+
+
+const repre =   document.getElementById('repre');
+const repreHandler = () =>{
+    document.getElementById('representativeImgSet').setAttribute('style', "display:grid");
+    document.getElementById('livingRoomImgSet').setAttribute('style', "display:none");
+    document.getElementById('kitchenImgSet').setAttribute('style', "display:none");
+    document.getElementById('seminarLoofImgSet').setAttribute('style', "display:none");
+};
+repre.addEventListener("click", repreHandler)
+
+
+const living =   document.getElementById('living');
+const livingHandler = () =>{
+    document.getElementById('representativeImgSet').setAttribute('style', "display:none");
+    document.getElementById('livingRoomImgSet').setAttribute('style', "display:grid");
+    document.getElementById('kitchenImgSet').setAttribute('style', "display:none");
+    document.getElementById('seminarLoofImgSet').setAttribute('style', "display:none");
+};
+living.addEventListener("click", livingHandler)
+
+
+
+const kitchen =   document.getElementById('kitchen');
+const kitchenHandler = () =>{
+    document.getElementById('representativeImgSet').setAttribute('style', "display:none");
+    document.getElementById('livingRoomImgSet').setAttribute('style', "display:none");
+    document.getElementById('kitchenImgSet').setAttribute('style', "display:grid");
+    document.getElementById('seminarLoofImgSet').setAttribute('style', "display:none");
+};
+kitchen.addEventListener("click", kitchenHandler)
+
+
+
+const semiloof =   document.getElementById('seminarLoof');
+const semiloofHandler = () =>{
+    document.getElementById('representativeImgSet').setAttribute('style', "display:none");
+    document.getElementById('livingRoomImgSet').setAttribute('style', "display:none");
+    document.getElementById('kitchenImgSet').setAttribute('style', "display:none");
+    document.getElementById('seminarLoofImgSet').setAttribute('style', "display:grid");
+};
+semiloof.addEventListener("click", semiloofHandler)
+
+
+
